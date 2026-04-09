3536ce864184c8a29f7c33e02bd814aab92d0c04
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-04-09 14:14:34 +0000

    ghjkl

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,52 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		String[][] maze = {
+      {"", "", "", "", ""},
+      {"", "*", "", "", ""},
+      {"", "*", "", "*", ""},
+      {"S", "*", "", "", "F"},
+    };
+
+	System.out.println(searchMazeMoves(maze));
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int searchMazeMoves(String[][] arr){
+		int row = arr.length-1;
+		int col = 0;
+		int out = sMMHelper(arr, row, col, 0);
+
+		//since I used Integer.MAX_VALUE instead of -1, we have to rectify it here, also ternary operators are sick as heck 
+		return (out == Integer.MAX_VALUE ? -1 : out);
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+	public static int sMMHelper(String[][] arr, int row, int col, int moves){
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+		//base case: out of bounds (both ways now because we're omnidirectional) or if arr[row][col] is a wall
+		if((col >= arr[0].length || col < 0) || (row < 0 || row >= arr.length) || arr[row][col].equals("*")){
+			return Integer.MAX_VALUE;
+		}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+		//base case: arr[row][col] is at the finish
+		if(arr[row][col].equals("F")){
+			return moves;
+		}
+		
+		//add wall to avoid moving backwards
+		arr[row][col] = "*";
 
-	public static void q5() {
-		//Write question 5 code here
+		//recursively call for every direction
+		int uMoves = sMMHelper(arr, row - 1, col, moves + 1);
+		int dMoves = sMMHelper(arr, row + 1, col, moves + 1);
+		int rMoves = sMMHelper(arr, row, col + 1, moves + 1);
+		int lMoves = sMMHelper(arr, row, col - 1, moves + 1);
+
+		//remove wall once paths have been found
+		arr[row][col] = "";
+
+		//return the smallest of the values (this is why the base case returns Integer.MAX_VALUE, it just makes this easier)
+		return Math.min(Math.min(uMoves, dMoves) , Math.min(rMoves, lMoves));
 	}
 
 }
